e888048c0df8a39f83444ffc780bd625180f4073
noheezybucket <seydinag023@gmail.com>
2024-08-03 12:06:22 +0000

    initial

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have the 'cinaye-burger' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cinaye-burger');
+    expect(app.title).toEqual('cburger');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, cinaye-burger');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, cburger');
   });
 });
